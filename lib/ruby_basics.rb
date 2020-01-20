056c80ff9148f017bbdf67b9a67910ad703fc5c9
MaryJay787 <josephmaryam.mj@gmail.com>
2020-01-19 19:15:14 -0500

    different ruby method practice

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -12,7 +12,7 @@ def argue(argument)
 end
 
 def greeting(greeting, name)
-
+  puts "#{greeting} my name is #{name}!"
 end
 
 def return_a_value
@@ -27,3 +27,17 @@ end
 def  pizza_party(pizza = "cheese")
 return "#{pizza}"
 end
+
+def ruby_basics
+  greeting("Hello", "Greg")
+  age = division(10, 5)
+  food = pizza_party
+  friend = assign_variable("Steve")
+  if age != 0
+    puts "When I was #{age}, I linked #{food}"
+    puts "My childhood friends name was #{friend}"
+  end
+
+end
+
+ruby_basics
